208971dc44c53f77ad1ec8cd44aafecae7061389
Georgy Chargaziya <georgy@Mac.lan>
2025-08-15 09:52:48 +0100

    changed path to front end v2

--- src/main/java/com/example/demo/controllers/SongController.java
+++ src/main/java/com/example/demo/controllers/SongController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@CrossOrigin(origins = "https://sleephawk.github.io/lyrics-front-end")
+@CrossOrigin(origins = "https://sleephawk.github.io/")
 @RequestMapping("/api/songs")
 public class SongController {
 
